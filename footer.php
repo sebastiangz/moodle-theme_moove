9d3b45b2bcbbbadfa393440c5a8fa9383d41f0a8
Sebastian Gonzalez Zepeda <sebastiangz@hotmail.com>
2020-06-25 12:54:38 -0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,129 @@
+                <div id="publicacion">
+        <div class="container">
+                        <div class="row">
+                                <div class="col-xl-12 col-lg-12 col-md-12 col-xs-12">
+                                        <small><em></em></small>
+                                </div>
+                        </div>
+        </div>
+    </div>
+    <div class="scrollup"><span class="icon-angle-up"></span></div>
+    <footer class="bd-footer text-muted" role="contentinfo">
+            <div class="container">
+                    <div class="row">
+                            <div id="tdireccion" class="col-xl-4 col-lg-3 col-md-6 col-xs-12">
+                                    <img src="//www.ucol.mx/cms/beta/img/icon/logo.svg" class="img-responsive logo-ucol"><br />
+                                    <img src="//www.ucol.mx/cms/beta/img/icon/ubicacion.svg" width="20"> Dirección: Av. Universidad No. 333, Las Víboras; CP 28040 Colima, Colima, México<br /><br />
+                                    <img src="//www.ucol.mx/cms/beta/img/icon/directorio.svg" width="20"> <a target="_blank" href="http://sistemas.ucol.mx/telefonia/">Directorio Telefónico</a><br />
+                                    <img src="//www.ucol.mx/cms/beta/img/icon/plantel.svg" width="20"> <a target="_blank" href="/conocenos/planteles-dependencias.htm">Planteles y Dependencias</a><br />
+                                    <img src="//www.ucol.mx/cms/beta/img/icon/sitio.svg" width="20"> <a href="/conocenos/mapa.htm">Mapa de Sitio</a><hr>
+                                        <img src="//www.ucol.mx/cms/beta/img/icon/transparencia.svg" width="20"> <a href="https://transparencia.ucol.mx/">Transparencia</a><br>
+                                        <img src="//www.ucol.mx/cms/beta/img/icon/candado.svg" width="20"> <a target="_blank" href="/conocenos/aviso-privacidad.htm">Aviso de privacidad</a><br /><br />
+                            </div>
+                            <div id="tconoce" class="col-xl-3 col-lg-3 col-md-6 col-xs-12">
+                                    CONOCE LA UNIVERSIDAD DE COLIMA
+                                    <ul class="bd-footer-link">
+                                            <li><a href="/conocenos/">Bienvenida</a></li>
+                                            <li><a href="/gobierno-universitario/">Gobierno Universitario</a></li>
+                                            <li><a href="/conocenos/memoria-universitaria.htm">Memoria Universitaria</a></li>
+                                            <li><a href="/marco-filosofico/">Marco Filosófico</a></li>
+                                            <li><a href="/conocenos/desarrollo-familia.htm">Familia Universitaria</a></li>
+                                    </ul>
+                                    CONOCE EL PORTAL UCOL
+                                    <ul class="bd-footer-link">
+                                                <li><a href="https://wayf.ucol.mx/federacion/" target="_blank">Federaci&oacute;n de Identidades de la Universidad de Colima</a></li>
+                                            <li><a href="/content/cms/12/file/fundamentacion.pdf" target="_blank">Información para Usuario</a></li>
+                                            <li><a href="https://drive.google.com/file/d/0B5DEEE9IJ3vQcXJtOXpmZTZIX2s/view" target="_blank">Manual de Usuario Planteles y Dependencias</a></li>
+                                    </ul>
+                                    <button type="button" class="btn btn-secondary-outline btn-ucol" id="demo" data-demo>Portal Interactivo</button><br /><br /><br /><br />
+                            </div>
+                            <div id="tredes" class="col-xl-2 col-lg-3 col-md-6 col-xs-12">
+                                    REDES SOCIALES
+                                    <ul class="bd-footer-link redes-horizontal">
+                                            <li><a href="https://www.facebook.com/pages/Universidad-de-Colima-Oficial/158697750839178" target="_blank"><img src="//www.ucol.mx/cms/beta/img/icon/facebook-b.svg" alt="Facebook" width="15"></a></li>
+                                            <!--<li><a href="https://google.com/+UcolMx/" target="_blank"><img src="//www.ucol.mx/cms/beta/img/icon/google-plus-b.svg" alt="Google Plus" width="15"></a></li>-->
+                                            <li><a href="https://twitter.com/udec_oficial" target="_blank"><img src="//www.ucol.mx/cms/beta/img/icon/twitter-b.svg" alt="Twitter" width="15"></a></li>
+                                            <li><a  target="_blank"  href="https://www.youtube.com/universidaddecolimatv"><img src="//www.ucol.mx/cms/beta/img/icon/youtube-b.svg" alt="Youtube" width="15"></a></li>
+                                            <!--<li><a href="https://www.flickr.com/photos/udecolima" target="_blank"><img src="//www.ucol.mx/cms/beta/img/icon/flickr-b.svg" alt="Flickr" width="15"></a></li>-->
+                                            <li><a href="https://www.instagram.com/udec.oficial/" target="_blank"><img src="//www.ucol.mx/cms/beta/img/icon/instagram-b.svg" alt="Flickr" width="15"></a></li>
+                                    </ul><br /><br /><br />
+                                    ENGLISH WEBSITE
+                                    <ul class="bd-footer-link redes-horizontal">
+                                            <li><a href="https://english.ucol.mx" target="_blank"><img class="img-icon" src="//www.ucol.mx/cms/beta/img/icon/ingles.svg" alt="Visit english website"></a></li>
+                                    </ul><br /><br />
+                                    <!--<img src="//www.ucol.mx/cms/beta/img/icon/pregunta-2.svg" alt="Preguntas Frecuentes" width="15"> <a href="/conocenos/construccion.htm">Preguntas Frecuentes</a><br /><br />
+                                    <img src="//www.ucol.mx/cms/beta/img/icon/atencion-2.svg" alt="Atención Universitaria" width="15"> <a href="/conocenos/construccion.htm">Atención Universitaria</a>-->
+                            </div>
+                            <div id="tlogos" class="col-xl-3 col-lg-3 col-md-6 col-xs-12">
+                                    <div class="block-center">
+                                            <a href="http://www.sep.gob.mx/" target="_blank"><img src="//www.ucol.mx/cms/beta/img/LogoSEP1.png" alt="SEP"></a><br><br>
+                                                <a href="http://www.anuies.mx/" target="_blank"><img src="//www.ucol.mx/cms/beta/img/anuies.png" alt="ANUIES"></a>
+                                                <a href="http://www.unesco.org/new/es/" target="_blank"><img src="//www.ucol.mx/cms/beta/img/unesco.png" alt="UNESCO"></a>
+                                                <a href="http://www.cumex.org.mx/" target="_blank"><img src="//www.ucol.mx/cms/beta/img/consorcio.png" alt="Consorcio"></a>
+                                                <hr />
+                                                <a href="//portal.ucol.mx/cs/" target="_blank" ><img src="//www.ucol.mx/cms/beta/img/contraloria.png"  alt="Contraloría"></a>
+                                                <a href="http://www.federaciondeestudiantescolimenses.com/" target="_blank"><img src="//www.ucol.mx/cms/beta/img/LogoFEC.png" alt="FEC" height="40"></a>
+                                                <a href="http://sutucoficial.org/" target="_blank"><img src="//www.ucol.mx/cms/beta/img/SUTUC.png" alt="SUTUC"></a><br><br>
+                                                <a href="//portal.ucol.mx/feuc/" target="_blank"><img src="//www.ucol.mx/cms/beta/img/feuc14.png" alt="FEUC" height="40"></a>
+                                                <a href="//portal.ucol.mx/jubilados/" target="_blank"><img src="//www.ucol.mx/cms/beta/img/logoJubilados.png" alt="Jubilados" height="40"></a><br><br>
+                                        </div>
+                            </div>
+                            <div class="col-xl-12 col-lg-12 col-md-12 col-xs-12 center-links">
+                                    &copy; Derechos Reservados 2018-2021 Universidad de Colima
+                                <br/>
+                                <br/>
+                                    <small>Este sitio podrá ser citado mientras no sea con fines de lucro, únicamente colocando su respectiva fuente y dirección electrónica.
+                                    <br/>De otra manera, se solicitará presentar un permiso previamente escrito de la respectiva institución.</small>
+                            </div>
+                    </div>
+            </div>
+        </footer>
+
+    <!-- Bootstrap tour CSS -->
+    <link href="//www.ucol.mx/cms/beta/dist/css/bootstrap-tour.min.css" rel="stylesheet">
+    <!-- JS Bootstrap -->
+    <script src="//www.ucol.mx/cms/beta/dist/js/tether.min.js" type="text/javascript"></script>
+    <script src="//www.ucol.mx/cms/beta/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="//www.ucol.mx/cms/beta/dist/js/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
+    <!-- JS adds -->
+    <script src="//www.ucol.mx/cms/beta/js/jquery.slides.js" type="text/javascript"></script>
+    <script src="//www.ucol.mx/cms/beta/js/jquery.flexisel0815.js" type="text/javascript"></script>
+    <script src="//www.ucol.mx/cms/beta/js/purl.js"></script>
+    <script src="//www.ucol.mx/cms/beta/js/custom.min.js?v=1.2" type="text/javascript"></script>
+    <!-- Bootstrap tour JS -->
+    <script src="//www.ucol.mx/cms/beta/js/bootstrap-tour.min.js"></script>
+    <script src="//www.ucol.mx/cms/beta/js/app2.min.js"></script>
+
+    <!-- HTML5 shim y Respond.js para soporte IE8 de elementos HTML5 y media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-3686571-1', 'auto');
+          /*ga('require', 'GTM-NKZPZL3');*/
+          ga('require', 'displayfeatures');
+          ga('send', 'pageview');
+
+          if ("serviceWorker" in navigator) {
+          if (navigator.serviceWorker.controller) {
+                console.log("Ya registrado.");
+          } else {
+                // Register the service worker
+                navigator.serviceWorker
+                  .register("/service-worker.js", {
+                        scope: "./"
+                  })
+                  .then(function (reg) {
+                        console.log("Service worker has been registered for scope: " + reg.scope);
+                  });
+          }
+         }
+        </script>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+        <!-- meta tags requeridos -->
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <meta name="author" content="Universidad de Colima"/>
+        <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:description" content="Universidad de Colima"/>
+        <meta property="og:image" content="http://wayf.ucol.mx/ucol_mini.png"/>
+        <meta property="og:locale:alternate" content="es_ES" />
+        <meta property="og:site_name" content="Universidad de Colima" />
+
+        <!-- Titulo principal -->
+        <title>Direcci&oacute;n General de Educaci&oacute;n Continua</title>
+        <!-- link requeridos -->
+        <link href="http://wayf.ucol.mx/ucol_mini.png" rel="image_src" />
+        <link href="//www.ucol.mx/cms/img/favicon.ico" type="image/x-icon" rel="icon" />
+        <!-- jQuery -->
+        <script src="//www.ucol.mx/cms/beta/js/jquery.min.js"></script>
+        <!-- Bootstrap y header & footer agregados -->
+        <link href="//www.ucol.mx/cms/headerfooterapp2.css" rel="stylesheet">
+        <link href="//www.ucol.mx/cms/beta/css/carrusel.css" rel="stylesheet">
+
+  </head>
+  <body>
+        <div id="estructura">
+        <!-- Fixed navbar -->
+        <nav class="navbar navbar-light bg-faded theme-primary pos-f-t">
+                <div class="container">
+                        <div class="row">
+                                <div class="col-xl-12 col-lg-12 col-md-12 col-xs-12">
+                                        <button class="navbar-toggler hidden-md-up pull-right collapsed" type="button" data-toggle="collapse" data-target="#navbar-header" aria-controls="navbar-header" aria-expanded="false">&#x2630;</button>                               <div class="collapse navbar-toggleable-sm" id="navbar-header">
+
+                                    <a class="navbar-brand" id="logo" href="//www.ucol.mx/">Universidad de Colima</a>
+                                    <span class="home-href"><a href="./">Direcci&oacute;n General de Educaci&oacute;n Continua</a></span>
+                                                                                   <!--<ul id="navlist" class="nav navbar-primary navbar-nav pull-md-right">
+                                                   <li class="nav-item">
+                                                      <a class="nav-link" data-hover="dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">
+                                                         Opcion 1
+                                                      </a>
+                                                           <ul id="subnavlist" class="dropdown-menu">
+                                                              <li><a href="#">Sub opcion 1</a></li>
+                                                              <li><a href="#">Sub opcion 2</a></li>
+                                                           </ul>
+                                                   </li>
+                                                   <li class="nav-item">
+                                                      <a class="nav-link" href="#">
+                                                         Opcion 2
+                                                      </a>
+                                                   </li>
+                                                   <li class="nav-item">
+                                                      <a class="nav-link" href="#">
+                                                         Opcion 3
+                                                      </a>
+                                                   </li>
+                                                </ul>-->
+                                </div>
+                                </div>
+                        </div>
+                </div>
+    </nav> <!-- /navbar -->
